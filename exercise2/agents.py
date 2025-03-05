2726f47ad23963dbf95703fef051ddaabe8ad270
Fionn <fionnwhelan1@gmail.com>
2025-03-05 16:16:05 +0000

    Initial commit

---
+++ exercise2/agents.py
@@ -0,0 +1,213 @@
+from abc import ABC, abstractmethod
+from collections import defaultdict
+import random
+from typing import List, Dict, DefaultDict
+from gymnasium.spaces import Space
+from gymnasium.spaces.utils import flatdim
+
+class Agent(ABC):
+    """Base class for Q-Learning agent
+
+    **ONLY CHANGE THE BODY OF THE act() FUNCTION**
+
+    """
+
+    def __init__(
+        self,
+        action_space: Space,
+        obs_space: Space,
+        gamma: float,
+        epsilon: float,
+        **kwargs
+    ):
+        """Constructor of base agent for Q-Learning
+
+        Initializes basic variables of the Q-Learning agent
+        namely the epsilon, learning rate and discount rate.
+
+        :param action_space (int): action space of the environment
+        :param obs_space (int): observation space of the environment
+        :param gamma (float): discount factor (gamma)
+        :param epsilon (float): epsilon for epsilon-greedy action selection
+
+        :attr n_acts (int): number of actions
+        :attr q_table (DefaultDict): table for Q-values mapping (OBS, ACT) pairs of observations
+            and actions to respective Q-values
+        """
+
+        self.action_space = action_space
+        self.obs_space = obs_space
+        self.n_acts = flatdim(action_space)
+
+        self.epsilon: float = epsilon
+        self.gamma: float = gamma
+
+        self.q_table: DefaultDict = defaultdict(lambda: 0)
+
+    def act(self, obs: int) -> int:
+        """Implement the epsilon-greedy action selection here
+
+        **YOU MUST IMPLEMENT THIS FUNCTION FOR Q2**
+
+        :param obs (int): received observation representing the current environmental state
+        :return (int): index of selected action
+        """
+        ### PUT YOUR CODE HERE ###
+        # obs a list of q?? values for each state. The observation is a value representing the player’s 
+        # current position as current_row * ncols + current_col (where both the row and col start at 0).
+        # For example, the goal position in the 4x4 map can be calculated as follows: 3 * 4 + 3 = 15. The 
+        # number of possible observations is dependent on the size of the map
+
+        if random.random() <= self.epsilon:
+            return random.randint(0, self.n_acts - 1)
+
+        best_q = float('-inf')
+        best_act = random.randint(0, self.n_acts - 1)
+        for act in range(self.n_acts):
+            q_act = self.q_table[(obs, act)]
+            if q_act > best_q:
+                best_act = act
+                best_q = q_act
+        
+
+        # raise NotImplementedError("Needed for Q2")
+        ### RETURN AN ACTION HERE ###
+        return best_act
+
+    @abstractmethod
+    def schedule_hyperparameters(self, timestep: int, max_timestep: int):
+        """Updates the hyperparameters
+
+        This function is called before every episode and allows you to schedule your
+        hyperparameters.
+
+        :param timestep (int): current timestep at the beginning of the episode
+        :param max_timestep (int): maximum timesteps that the training loop will run for
+        """
+        ...
+
+    @abstractmethod
+    def learn(self):
+        ...
+
+
+class QLearningAgent(Agent):
+    """Agent using the Q-Learning algorithm"""
+
+    def __init__(self, alpha: float, **kwargs):
+        """Constructor of QLearningAgent
+
+        Initializes some variables of the Q-Learning agent, namely the epsilon, discount rate
+        and learning rate alpha.
+
+        :param alpha (float): learning rate alpha for Q-learning updates
+        """
+
+        super().__init__(**kwargs)
+        self.alpha: float = alpha
+
+    def learn(
+        self, obs: int, action: int, reward: float, n_obs: int, done: bool
+    ) -> float:
+        """Updates the Q-table based on agent experience
+
+        **YOU MUST IMPLEMENT THIS FUNCTION FOR Q2**
+
+        :param obs (int): received observation representing the current environmental state
+        :param action (int): index of applied action
+        :param reward (float): received reward
+        :param n_obs (int): received observation representing the next environmental state
+        :param done (bool): flag indicating whether a terminal state has been reached
+        :return (float): updated Q-value for current observation-action pair
+        """
+        ### PUT YOUR CODE HERE ###
+
+        
+        best_q = float('-inf')
+        for act in range(self.n_acts):
+            q_act = self.q_table[(n_obs, act)]
+            if q_act > best_q:
+                best_q = q_act
+        
+        if done:
+            best_q = 0
+
+
+        self.q_table[(obs, action)] = self.q_table[(obs, action)] + self.alpha*(reward + self.gamma*best_q - self.q_table[(obs, action)])
+        # print(self.q_table[(obs, action)])
+        # raise NotImplementedError("Needed for Q2")
+        return self.q_table[(obs, action)]
+
+    def schedule_hyperparameters(self, timestep: int, max_timestep: int):
+        """Updates the hyperparameters
+
+        **DO NOT CHANGE THE PROVIDED SCHEDULING WHEN TESTING PROVIDED HYPERPARAMETER PROFILES IN Q2**
+
+        This function is called before every episode and allows you to schedule your
+        hyperparameters.
+
+        :param timestep (int): current timestep at the beginning of the episode
+        :param max_timestep (int): maximum timesteps that the training loop will run for
+        """
+        self.epsilon = 1.0 - (min(1.0, timestep / (0.20 * max_timestep))) * 0.99
+
+
+class MonteCarloAgent(Agent):
+    """Agent using the Monte-Carlo algorithm for training"""
+
+    def __init__(self, **kwargs):
+        """Constructor of MonteCarloAgent
+
+        Initializes some variables of the Monte-Carlo agent, namely epsilon,
+        discount rate and an empty observation-action pair dictionary.
+
+        :attr sa_counts (Dict[(Obs, Act), int]): dictionary to count occurrences observation-action pairs
+        """
+        super().__init__(**kwargs)
+        self.sa_counts = {}
+        self.num_returns = defaultdict(lambda: 0)
+
+    def learn(
+        self, obses: List[int], actions: List[int], rewards: List[float]
+    ) -> Dict:
+        """Updates the Q-table based on agent experience
+
+        **YOU MUST IMPLEMENT THIS FUNCTION FOR Q2**
+
+        :param obses (List(int)): list of received observations representing environmental states
+            of trajectory (in the order they were encountered)
+        :param actions (List[int]): list of indices of applied actions in trajectory (in the
+            order they were applied)
+        :param rewards (List[float]): list of received rewards during trajectory (in the order
+            they were received)
+        :return (Dict): A dictionary containing the updated Q-value of all the updated state-action pairs
+            indexed by the state action pair.
+        """
+        # From docstring, presume we just update the q_table directly, so don't need additional dictionary
+        # updated_values = {}
+        
+        G = 0
+        for t in reversed(range(len(obses))):
+            self.num_returns[(obses[t], actions[t])] += 1
+            G = self.gamma*G + rewards[t]
+            # returns[(obses[t], actions[t])].append(G)
+            # print(returns)
+
+            self.q_table[(obses[t], actions[t])] += (G - self.q_table[(obses[t], actions[t])]) / self.num_returns[(obses[t], actions[t])]
+        
+        ### PUT YOUR CODE HERE ###
+        # raise NotImplementedError("Needed for Q2")
+        return self.q_table
+
+    def schedule_hyperparameters(self, timestep: int, max_timestep: int):
+        """Updates the hyperparameters
+
+        **DO NOT CHANGE THE PROVIDED SCHEDULING WHEN TESTING PROVIDED HYPERPARAMETER PROFILES IN Q2**
+
+        This function is called before every episode and allows you to schedule your
+        hyperparameters.
+
+        :param timestep (int): current timestep at the beginning of the episode
+        :param max_timestep (int): maximum timesteps that the training loop will run for
+        """
+        self.epsilon = 1.0 - (min(1.0, timestep / (0.9 * max_timestep))) * 0.8
